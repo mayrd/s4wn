d247656c07b54d94d0340a85945e1465b2ad88e1
Daniel Mayr <dg.mayr@gmail.com>
2026-07-04 12:07:57 +0200

    Delete bin-shim directory

--- bin-shim/test_asm_abs.rs
+++
@@ -1,9 +0,0 @@
-#![no_std]
-#![no_main]
-
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
-
-core::arch::global_asm!(include_str!("c:/Users/mayx/code/s4wn/bin-shim/test.s"));
